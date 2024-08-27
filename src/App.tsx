08ae0bf608f9515e0ff1ffbdaec9e79d9ba26d9d
gabrielMella <gabriel.angelo@cd2.com.br>
2024-08-27 12:37:55 -0300

    feat: creaet project base and history page

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './components/styles/themes/default'
+import { GlobalStyle } from './components/styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
